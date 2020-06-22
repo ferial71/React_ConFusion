61762ac9051d5c0c0be23ae732beeb5eb6240f4e
ferial71 <ff_bouzoul@esi.dz>
2020-06-22 23:18:23 +0100

    react component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import {Navbar,NavbarBrand} from 'reactstrap';
+import Menu from './components/MenuComponent';
 import './App.css';
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
         </div>
 
       </Navbar>
+        <Menu />
     </div>
   );
 }
